6abbe8aababb00db31d568fedda2a7588ef606ee
Mugipoppin <m.tmg1018@gmail.com>
2024-10-30 23:57:14 +0900

    jsファイル作成

---
+++ js/function.js
@@ -0,0 +1,30 @@
+function lot() { 
+    const data = { 
+      '1': 25,  // 25% 
+      '2': 20,  // 20% 
+      '3': 17, // 17% 
+      '4': 14, // 14% 
+      '5': 11, // 11% 
+      '6': 8, // 8% 
+      '7': 5, // 5% 
+    } 
+    const rand = Math.floor(Math.random() * 100) 
+    let result = 'はずれ'
+    let rate = 0 
+    for (const prop in data) { 
+      rate += data[prop] 
+      if (rand <= rate) { 
+        result = prop 
+        break
+      } 
+    } 
+    return result 
+    // 1等や2等などを設定した確率で表示 
+  } 
+    
+  const num1 = document.getElementById('num1') 
+  
+  let _r = [] 
+  b.addEventListener('click', () => { 
+    r.innerHTML = lot() 
+  })
